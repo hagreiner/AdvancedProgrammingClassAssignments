e16dd844c1c010c5d1776ec959936dfc638b6baa
hagreiner <47033071+hagreiner@users.noreply.github.com>
2020-01-17 15:17:00 -0500

    comments

--- Lab1.1_2DArrays/Lab1.1_2DArrays/functions.h
+++ Lab1.1_2DArrays/Lab1.1_2DArrays/functions.h
@@ -14,14 +14,43 @@ using namespace std;
 
 const int INFO = 2;
 
-//load language names from a file into the names array - return number of languages read
+ /**
+  * @brief load language names from a file into the names array - return number of languages read
+  * @author Bilbo B. Baggins
+  * @assignment Lab 1.1_2DArrays
+  * @date 12/16/2019
+  * @param filename - the name of the file being open and read in
+  * @param names[] - an empty array that will be loaded with the names from the file
+  * @param maxLanguages - the maximum number of languages that will be read in
+  * @return int - the number of languages found
+  */
 int loadLanguages( const string& filename, string names[], int maxLanguages );
 
-//load language info from a file into a 2d array of ints, return true if successfully processed
+ /**
+  * @brief load language info from a file into a 2d array of ints, return true if successfully processed
+  * @author Hannah Greiner
+  * @assignment Lab 1.1_2DArrays
+  * @date 12/16/2019
+  * @param filename - the name of the file being open and read in
+  * @param infoArray[][INFO] - 2D array that the information from the file is being loaded into
+  * @param numLanguages - the number of items found in the previous file
+  * @return bool - true if info is processed
+  * @credit Bilbo B. Baggins
+  */
 bool loadInfo( const string& filename, int infoArray[][INFO], int numLanguages );
 
-//formats and returns a string which contains the language name and the info for
-//a particular language (indicated by languageRank)
+/**
+  * @brief formats and returns a string which contains the language name and the info for
+  a particular language (indicated by languageRank)
+  * @author Hannah Greiner
+  * @assignment Lab 1.1_2DArrays
+  * @date 12/16/2019
+  * @param languageRank - corresponds to the position in the arrays where information will be called from
+  * @param infoArray[][INFO] - 2D array with the data for year and rank
+  * @param names[] - an array that contains the names of the languages
+  * @return string - contains the language, rank, and year
+  * @credit Bilbo B. Baggins
+  */
 string formatReportLine( int languageRank, int infoArray[][INFO], string names[] );
 
 #endif /* defined(__lab1__functions__) */
